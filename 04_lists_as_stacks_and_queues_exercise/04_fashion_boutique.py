0d850f3cc42e0bdb7fc50a7c5eff464104e643a1
wako8o <emilaleksandrov11@gmail.com>
2025-09-16 00:30:52 +0200

    new

--- 04_lists_as_stacks_and_queues_exercise/04_fashion_boutique.py
+++ 04_lists_as_stacks_and_queues_exercise/04_fashion_boutique.py
@@ -6,12 +6,30 @@ capacity_clothes = capacity
 
 while box_of_clothes:
     clothes = box_of_clothes.pop()
+    capacity_clothes += clothes
 
-    if capacity_clothes >= clothes:
-        capacity_clothes -= clothes
+    if capacity >= capacity_clothes:
+        pass
 
     else:
+        box_of_clothes.append(clothes)
         box += 1
-        capacity_clothes = capacity - clothes
+        capacity_clothes = 0
 
 print(box)
+
+
+# second approach with subtraction
+
+
+# while box_of_clothes:
+#     clothes = box_of_clothes.pop()
+#
+#     if capacity_clothes >= clothes:
+#         capacity_clothes -= clothes
+#
+#     else:
+#         box += 1
+#         capacity_clothes = capacity - clothes
+#
+# print(box)
---
+++ 04_lists_as_stacks_and_queues_exercise/07_robotics.py
@@ -0,0 +1,73 @@
+from collections import deque
+from datetime import datetime, timedelta
+
+
+data = input().split(";")
+robots = {}
+for info in data:
+    name, process_time = info.split("-")
+    robots[name] = {"time": int(process_time), "free_at": None}
+
+start_time = datetime.strptime(input(), "%H:%M:%S")
+products = deque()
+
+product = input()
+while product != "End":
+    products.append(product)
+    product = input()
+
+time = start_time
+
+while products:
+    time += timedelta(seconds=1)
+    current_product = products.popleft()
+
+    free_robot = None
+    for name, info in robots.items():
+        if info["free_at"] is None or info["free_at"] <= time:
+            free_robot = name
+            break
+
+    if free_robot:
+        robots[free_robot]["free_at"] = time + timedelta(seconds=robots[free_robot]["time"])
+        print(f"{free_robot} - {current_product} [{time.strftime('%H:%M:%S')}]")
+    else:
+        products.append(current_product)
+
+
+
+# from collections import deque
+# from datetime import datetime, timedelta
+#
+# data = input().split(";")
+# robots = {}
+# products = deque()
+# time = datetime.strptime(input(), "%H:%M:%S") + timedelta(seconds=1)
+# product = input()
+#
+# for info in data:
+#     info = info.split("-")
+#     robots[info[0]] = [int(info[1]), time]
+#
+# free_robots = deque(robots)
+#
+# while product != "End":
+#     products.append(product)
+#     product = input()
+#
+# while products:
+#     current_product = products.popleft()
+#     if free_robots:
+#         robot = free_robots.popleft()
+#         robots[robot][1] = time + timedelta(seconds=robots[robot][0])
+#         print(f"{robot} - {current_product} [{time.strftime('%H:%M:%S')}]")
+#         for r in robots:
+#             if r not in free_robots:
+#                 if time + timedelta(seconds=1) >= robots[r][1]:
+#                     free_robots.append(r)
+#     else:
+#         for r in robots:
+#             if time + timedelta(seconds=1) >= robots[r][1]:
+#                 free_robots.append(r)
+#         products.append(current_product)
+#     time += timedelta(seconds=1)
